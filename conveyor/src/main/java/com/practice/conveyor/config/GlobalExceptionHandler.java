a35bbdeb7d9a0b7e1a341f5007671e3d44b6d4dc
Metod-OverDrive <pochta.nadomne@mail.ru>
2024-12-26 16:26:20 +0300

    Connect conveyor service to ELK

--- conveyor/src/main/java/com/practice/conveyor/config/GlobalExceptionHandler.java
+++ conveyor/src/main/java/com/practice/conveyor/config/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.practice.conveyor.config;
 
 import com.practice.conveyor.web.exception.RejectApplicationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,10 +12,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        log.error(ex.getMessage());
         Map<String, String> errors = new HashMap<>();
 
         for (FieldError error : ex.getBindingResult().getFieldErrors()) {
@@ -26,7 +29,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(RejectApplicationException.class)
     public ResponseEntity<Map<String, String>> handleRejectExceptions(RejectApplicationException ex) {
-
+        log.error(ex.getMessage());
         return ResponseEntity.badRequest().body(Map.of("Error", ex.getMessage()));
     }
 
